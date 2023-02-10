50d3655740b75d3920338c33aa51ce25d70fa430
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-02-09 22:58:08 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/fabioaacarneiro/course/config/TestConfig.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fabioaacarneiro.course.config;
+
+import com.fabioaacarneiro.course.entities.User;
+import com.fabioaacarneiro.course.repositories.UserRepository;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
+ *
+ * @author fabio
+ */
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+        User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+        
+        userRepository.saveAll(Arrays.asList(u1, u2));
+    }
+}
