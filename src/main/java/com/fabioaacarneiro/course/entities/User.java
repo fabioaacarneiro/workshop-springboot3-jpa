5a78a6455bbcb761b00528328a1d52e6eb50ce16
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-02-26 15:15:36 -0300

    User update

--- src/main/java/com/fabioaacarneiro/course/entities/User.java
+++ src/main/java/com/fabioaacarneiro/course/entities/User.java
@@ -4,19 +4,17 @@
  */
 package com.fabioaacarneiro.course.entities;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 /**
  *
@@ -59,16 +57,32 @@ public class User implements Serializable {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+    
     public String getEmail() {
         return email;
     }
-
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+   
     public String getPhone() {
         return phone;
     }
 
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
     public String getPassword() {
         return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public List<Order> getOrders() {
